48be03698b9912b8f2be2718ad3001b8f9880354
derick871 <wanyamaderrick4@gmail.com>
2026-08-16 13:04:47 +0300

    fix: correct the typo and add debug on context_processors

--- Backend/county_service_tracker/settings.py
+++ Backend/county_service_tracker/settings.py
@@ -80,11 +80,12 @@ ROOT_URLCONF = 'county_service_tracker.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
