43cfe0208cf21b98a9384fec0d8c6b15ca220596
derick871 <wanyamaderrick4@gmail.com>
2026-09-02 19:13:48 +0300

    feat(payments): add InitiatePaymentview API endpoint skeleton

--- Backend/Payments/views.py
+++ Backend/Payments/views.py
@@ -10,3 +10,10 @@ from.utils.pdf_generator import generate_payment_pdf
 from.utils.mpesa import get_mpesa_token, stk_push
 
 # Create your views here.
+class InitiatePaymentView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        phone = request.data.get('phone_number')
+        amount = request.data.get('amount')
+        tracking = request.data.get('tracking_number')
