ad759f180f31b95ed1256963b6fc8b7e63417e2e
derick871 <wanyamaderrick4@gmail.com>
2026-09-02 23:01:16 +0300

    feat: import the necessary modules

---
+++ Backend/Payments/utils/pdfGenerator.py
@@ -0,0 +1,8 @@
+import os
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet
+from django.conf import settings
+from django.core.files 
+import File
