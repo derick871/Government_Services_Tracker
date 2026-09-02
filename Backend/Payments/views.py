25da4daf6061fc5d5278707641bf1f9ffd4f5cc7
derick871 <wanyamaderrick4@gmail.com>
2026-09-02 19:12:27 +0300

    feat(payments): Import necessary modules for requests, base64, and PDF handling.

--- Backend/Payments/views.py
+++ Backend/Payments/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import base64, json, requests
+from django.conf import settings
+from django.http import FileResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from.models import Payment
+from.utils.pdf_generator import generate_payment_pdf
+from.utils.mpesa import get_mpesa_token, stk_push
 
 # Create your views here.
