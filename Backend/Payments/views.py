2009057aebb6ebfdfcffab25f2d54b2c7da30639
derick871 <wanyamaderrick4@gmail.com>
2026-09-03 14:50:41 +0300

    fix: correct the typo of the module from the utils

--- Backend/Payments/views.py
+++ Backend/Payments/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from.models import Payment
-from.utils.pdf_generator import generate_payment_pdf
+from.utils.pdfGenerator import generate_payment_pdf
 from.utils.mpesa import get_mpesa_token, stk_push
 
 # Create your views here.
